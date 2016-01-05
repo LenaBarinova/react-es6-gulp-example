955513457993a2e5ee1cf3c5b575ca01cd2dba0c
JelenaBarinova <fiodorova.jelena@gmail.com>
2016-01-04 16:32:08 -0800

    React component changed

--- app.jsx
+++ app.jsx
@@ -5,8 +5,9 @@ let About = React.createClass ({
     return (
       <div>
         <div>
-          <h1>Hello I'm here</h1>
-          <p> Check this out </p>
+          <h1>Hello!</h1>
+          <h2>This text was created as React component.</h2>
+          <p>For more information please visit my blog at <a href="http://www.bebetterdeveloper.com/coding/es6-react-babel.html" target="blank">BeBetterDeveloper</a></p>
         </div>
       </div>
     );
